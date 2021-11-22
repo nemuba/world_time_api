ef1126891c122a51c861fd952afbbe432f1d4499
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2021-11-22 11:57:26 -0300

    Add methods time and timezones

--- lib/world_time_api.rb
+++ lib/world_time_api.rb
@@ -1,8 +1,29 @@
 # frozen_string_literal: true
 
 require_relative "world_time_api/version"
+require "httparty"
 
+# module WorldTimeAPi
 module WorldTimeApi
-  class Error < StandardError; end
-  # Your code goes here...
+  include HTTParty
+
+  base_uri "http://worldtimeapi.org/api/timezone"
+
+  def self.timezones
+    response = get("/")
+    return "Consult Error" if response.code != 200
+
+    response
+  rescue Net::OpenTimeout
+    "Error time out connection"
+  end
+
+  def self.time(zone_name)
+    response = get("/#{zone_name}")
+    return "Invalid Zone Name" if response.code != 200
+
+    response
+  rescue Net::OpenTimeout
+    "Error time out connection"
+  end
 end
